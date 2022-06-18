c2476498f2a39eb18ca19362adec0eda5ff0738f
Arseniy Khvorov <khvorov45@gmail.com>
2022-06-18 12:45:39 +1000

    Revert "change how timed sections work"
    This reverts commit 303e2edf838dcb59fd0bb1cd06209201d704f13f.

--- code/rorgame.zig
+++ code/rorgame.zig
@@ -41,24 +41,33 @@ pub fn main() !void {
     var temp_frame_index: usize = 0;
     var frame_index: usize = 0;
 
-    var timed_sections = time.Sections{.storage = undefined};
+    const TimedSectionID = enum {
+        input,
+        update,
+        clear_buffers,
+        animation,
+        debug_atlases,
+        debug_timings,
+        display_pixels,
+    };
+
+    var timed_sections: std.enums.EnumArray(TimedSectionID, time.Section) = undefined;
 
     while (window.is_running) {
-        timed_sections.used = 0;
 
         //
         // SECTION Input
         //
 
-        timed_sections.begin("input");
+        timed_sections.getPtr(.input).begin();
         window.pollForInput(&input);
-        timed_sections.endLast();
+        timed_sections.getPtr(.input).end();
 
         //
         // SECTION Update
         //
 
-        timed_sections.begin("update");
+        timed_sections.getPtr(.update).begin();
 
         if (input.pressed(.f5)) {
             assets_arena.used = 0;
@@ -76,17 +85,17 @@ pub fn main() !void {
         rect_topleft_x += 0.0;
         rect_topleft_y += 0.0;
 
-        timed_sections.endLast();
+        timed_sections.getPtr(.update).end();
 
         //
         // SECTION Render
         //
 
-        timed_sections.begin("clear_buffers");
+        timed_sections.getPtr(.clear_buffers).begin();
         renderer.clearBuffers();
-        timed_sections.endLast();
+        timed_sections.getPtr(.clear_buffers).end();
 
-        timed_sections.begin("animation");
+        timed_sections.getPtr(.animation).begin();
 
         const req_group = assets.texture_groups.get(.commando_walk);
         const req_tex = req_group[frame_index];
@@ -99,20 +108,22 @@ pub fn main() !void {
             req_tex,
         );
 
-        timed_sections.endLast();
+        timed_sections.getPtr(.animation).end();
 
-        timed_sections.begin("debug_atlases");
+        timed_sections.getPtr(.debug_atlases).begin();
         renderer.drawWholeAtlas(math.V2f{.x = 500, .y = 200});
-        timed_sections.endLast();
+        timed_sections.getPtr(.debug_atlases).end();
 
-        timed_sections.begin("debug_timings");
+        timed_sections.getPtr(.debug_timings).begin();
 
         {
             var y_offset: f32 = 0;
+            var iter = timed_sections.iterator();
             var total_ms: f32 = 0;
-            for (timed_sections.storage[0..timed_sections.used]) |section| {
+            while (iter.next()) |entry| {
+                const section = entry.value;
                 var buf: [64]u8 = undefined;
-                const text = try std.fmt.bufPrint(buf[0..], "{s}: {d:.3}", .{section.name, section.ms});
+                const text = try std.fmt.bufPrint(buf[0..], "{s}: {d:.3}", .{@tagName(entry.key), section.ms});
                 renderer.drawTextline(text, math.V2f{.x = 0, .y = 100 + y_offset}, math.Color{.r = 1, .g = 1, .b = 1, .a = 1});
                 y_offset += @intToFloat(f32, renderer.font.px_height_line);
                 total_ms += section.ms;
@@ -122,10 +133,10 @@ pub fn main() !void {
             renderer.drawTextline(text, math.V2f{.x = 0, .y = 100 + y_offset}, math.Color{.r = 1, .g = 1, .b = 1, .a = 1});
         }
 
-        timed_sections.endLast();
+        timed_sections.getPtr(.debug_timings).end();
 
-        timed_sections.begin("display_pixels");
+        timed_sections.getPtr(.display_pixels).begin();
         window.displayPixels(renderer.draw_buffer.pixels, renderer.draw_buffer.dim);
-        timed_sections.endLast();
+        timed_sections.getPtr(.display_pixels).end();
     }
 }
--- code/time.zig
+++ code/time.zig
@@ -1,37 +1,16 @@
 const builtin = @import("builtin");
-
-const assert = @import("std").debug.assert;
-
 const platform = if (builtin.os.tag == .windows) @import("time_windows.zig") else @panic("missing time implementation");
 usingnamespace platform;
 
 pub const Section = struct {
-    name: []const u8,
     start: platform.Clock,
     ms: f32,
 
-    pub fn begin(section: *Section, name: []const u8) void {
+    pub fn begin(section: *Section) void {
         section.start = platform.getCurrentClock();
-        section.name = name;
     }
 
     pub fn end(section: *Section) void {
         section.ms = platform.getMsFrom(section.start);
-    }
-};
-
-pub const Sections = struct {
-    storage: [64]Section,
-    used: usize = 0,
-
-    pub fn begin(sections: *Sections, name: []const u8) void {
-        assert(sections.used < sections.storage.len);
-        sections.storage[sections.used].begin(name);
-    }
-
-    pub fn endLast(sections: *Sections) void {
-        assert(sections.used < sections.storage.len);
-        sections.storage[sections.used].end();
-        sections.used += 1;
     }
 };
